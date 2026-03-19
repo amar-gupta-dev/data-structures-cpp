baf86fac6d45b3a8ac1442dea93fa31c62a3a607
amar gupta <amargupta8970@gmail.com>
2026-03-19 22:20:41 +0530

    Implement StockSpanner class for online stock span

---
+++ monoatomic stack/901_online_stock_span.cpp
@@ -0,0 +1,30 @@
+// Leetcode 901
+// Problem: online stock span
+// Topic: stack
+// Diff: midium
+
+class StockSpanner {
+public:
+    stack<pair<int,int>> st;
+
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+        int span = 1;
+
+        while(!st.empty() && st.top().first <= price) {
+            span += st.top().second;
+            st.pop();
+        }
+        st.push({price, span});
+        return span;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
